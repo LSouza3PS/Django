ac25b9da6d7d13549377bbda11a8de0c0f0e67a7
Leandro Benedito de Souza <leandrosouza1106@gmail.com>
2024-03-14 15:49:14 -0300

    Atualização de estilos e reestruturação

--- ERP/urls.py
+++ ERP/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from modules.views import home
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('gestao-de-tarefas/', admin.site.urls, name='admin'),
+    path('', home, name='home')
 ]
