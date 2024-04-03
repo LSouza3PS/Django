0959b9d661fe99270d57c37c5b751f306c4eac60
Leandro Benedito de Souza <leandrosouza1106@gmail.com>
2024-04-03 16:54:55 -0300

    Finalização do projeto

--- modules/admin.py
+++ modules/admin.py
@@ -5,13 +5,13 @@ from .models import Cliente, Servico, SolicitacaoServico, ItemSolicitacao, Outro
 class ClienteAdmin(admin.ModelAdmin):
     fieldsets = (
         ('Informações Pessoais', {
-            'fields': ('codigo', 'tipo_pessoa', 'cpf_cnpj', 'rg_ie', 'nome', 'sexo', 'carteira_trabalho', 'data_nascimento'),
+            'fields': ('tipo_pessoa', 'cpf_cnpj', 'rg_ie', 'nome', 'sexo', 'carteira_trabalho', 'data_nascimento'),
         }),
         ('Informações de Contato', {
-            'fields': ('telefone', 'telefone2', 'celular', 'email', 'fax'),
+            'fields': ('celular', 'email', 'telefone', 'telefone2', 'fax'),
         }), 
         ('Endereço', {
-            'fields': ('cep', 'rua_av', 'numero_rua_av', 'bairro', 'pais', 'uf', 'cidade_municipio', 'complemento'),
+            'fields': ('rua_av', 'numero_rua_av', 'complemento', 'bairro', 'pais', 'uf', 'cidade_municipio', 'cep'),
         }),
     )
     list_display = ['codigo', 'tipo_pessoa', 'cpf_cnpj', 'rg_ie', 'nome', 'sexo', 'carteira_trabalho', 'data_nascimento', 'telefone', 'telefone2', 'celular', 'email', 'fax', 'cep', 'rua_av', 'numero_rua_av', 'bairro', 'pais', 'uf', 'cidade_municipio', 'complemento']
@@ -19,7 +19,7 @@ class ClienteAdmin(admin.ModelAdmin):
     list_filter = ('tipo_pessoa', 'sexo', 'bairro', 'pais', 'uf', 'cidade_municipio')
 
 class ServicoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'descricao', 'preco')
+    list_display = ('nome', 'descricao')
     search_fields = ['nome']
 
 class ItemSolicitacaoInline(admin.TabularInline):
@@ -28,8 +28,8 @@ class ItemSolicitacaoInline(admin.TabularInline):
 
 class SolicitacaoServicoAdmin(admin.ModelAdmin):
     inlines = [ItemSolicitacaoInline]
-    list_display = ('cliente', 'servico', 'status', 'data_solicitacao')
-    list_filter = ('status', 'data_solicitacao')
+    list_display = ('cliente', 'servico', 'status', 'data_entrega', 'data_solicitacao')
+    list_filter = ('status', 'data_solicitacao', 'data_entrega')
     search_fields = ['cliente__username', 'servico__nome']
 
 class OutroModuloAdmin(admin.ModelAdmin):
--- modules/models.py
+++ modules/models.py
@@ -33,16 +33,16 @@ UF_CHOICES = [
 
 # Create your models here.
 class Cliente(models.Model):
-    codigo = models.CharField(max_length=20, unique=True, verbose_name='Código')
+    codigo = models.AutoField(primary_key=True, verbose_name='Código do Cliente')
     tipo_pessoa = models.CharField(max_length=1, choices=[('F', 'Física'), ('J', 'Jurídica')], verbose_name='Tipo de Pessoa')
     cpf_cnpj = models.CharField(max_length=18, unique=True, verbose_name='CPF/CNPJ')
-    rg_ie = models.CharField(max_length=15, blank=True, null=True, verbose_name='RG/IE')
+    rg_ie = models.CharField(max_length=15, verbose_name='RG/IE')
     nome = models.CharField(max_length=100, verbose_name='Nome')
-    sexo = models.CharField(max_length=1, choices=[('M', 'Masculino'), ('F', 'Feminino'), ('O', 'Outro')], verbose_name='Sexo')
+    sexo = models.CharField(max_length=1, choices=[('M', 'Masculino'), ('F', 'Feminino')], verbose_name='Sexo')
     carteira_trabalho = models.CharField(max_length=20, blank=True, null=True, verbose_name='Carteira de Trabalho')
-    data_nascimento = models.DateField(blank=True, null=True, verbose_name='Data de Nascimento')
-    insc_municipal = models.CharField(max_length=20, blank=True, null=True, verbose_name='Inscrição Municipal')
-    razao_social = models.CharField(max_length=100, blank=True, null=True, verbose_name='Razão Social')
+    data_nascimento = models.DateField(verbose_name='Data de Nascimento')
+    insc_municipal = models.CharField(max_length=20, verbose_name='Inscrição Municipal')
+    razao_social = models.CharField(max_length=100, verbose_name='Razão Social')
     cep = models.CharField(max_length=10, verbose_name='CEP')
     rua_av = models.CharField(max_length=255, verbose_name='Rua/Avenida')
     numero_rua_av = models.CharField(max_length=10, verbose_name='Número')
@@ -51,50 +51,53 @@ class Cliente(models.Model):
     uf = models.CharField(max_length=2, choices=UF_CHOICES, verbose_name='UF')
     cidade_municipio = models.CharField(max_length=100, verbose_name='Cidade/Município')
     complemento = models.CharField(max_length=255, blank=True, null=True, verbose_name='Complemento')
-    telefone = models.CharField(max_length=15, blank=True, null=True, verbose_name='Telefone')
-    telefone2 = models.CharField(max_length=15, blank=True, null=True, verbose_name='Telefone 2')
-    celular = models.CharField(max_length=15, blank=True, null=True, verbose_name='Celular')
+    telefone = models.CharField(max_length=15, blank=True, null=True, verbose_name='Telefone p/ Contato')
+    telefone2 = models.CharField(max_length=15, blank=True, null=True, verbose_name='Telefone p/ Contato 2')
+    celular = models.CharField(max_length=15, verbose_name='Celular')
     email = models.EmailField(unique=True, verbose_name='E-mail')
     fax = models.CharField(max_length=15, blank=True, null=True, verbose_name='Fax')
     
 
     def __str__(self):
-        return f"{self.codigo} - {self.nome}"
+        return f"{self.nome}"
     
     
     class Meta:
         verbose_name = 'Cliente'
+        verbose_name_plural = ' Clientes'
 
 class Servico(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.TextField()
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.nome
     
     class Meta:
         verbose_name = "Serviço"
+        verbose_name_plural = " Serviços"
 
 class SolicitacaoServico(models.Model):
     STATUS_CHOICES = (
+        ('Não Iniciado', 'Não Iniciado'),
         ('Pendente', 'Pendente'),
         ('Em Andamento', 'Em Andamento'),
         ('Concluído', 'Concluído'),
         ('Cancelado', 'Cancelado'),
     )
 
-    cliente = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    servico = models.ForeignKey(Servico, on_delete=models.CASCADE)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pendente')
-    data_solicitacao = models.DateTimeField(default=timezone.now)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, verbose_name="Cliente")
+    servico = models.ForeignKey(Servico, on_delete=models.CASCADE, verbose_name="Serviço")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Não Iniciado', verbose_name="Status")
+    data_entrega = models.DateTimeField(default=timezone.now, verbose_name="Data de Entrega")
+    data_solicitacao = models.DateTimeField(default=timezone.now, verbose_name="Data da Solicitação", editable=False)
 
     def __str__(self):
-        return f"Solicitação de {self.servico.nome} por {self.cliente.username}"
+        return f"Solicitação de {self.servico.nome} por {self.cliente.nome}"
     
     class Meta:
         verbose_name = "Solicitação de Serviço"
-        verbose_name_plural = "Solicitações de Serviço"
+        verbose_name_plural = " Solicitações de Serviço"
 
 class ItemSolicitacao(models.Model):
     solicitacao = models.ForeignKey(SolicitacaoServico, on_delete=models.CASCADE)
